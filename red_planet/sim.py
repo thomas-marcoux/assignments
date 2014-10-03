553eb2eb860741228ee46f22fe07e97776dfcad1
thomas-marcoux <thomas.marcoux.graphi@gmail.com>
2014-10-03 13:13:32 -0500

    Red planet update

--- red_planet/sim.py
+++ red_planet/sim.py
@@ -6,38 +6,51 @@ G = 0.7
 maxRange = 5.0
 
 class   Point:
-    def __init__(self, dist, angle, vel):
+    def __init__(self, dist, angle):
         self.dist = dist
         self.angle = angle
-        self.vel = vel
         self.setCartesianCoord()
 
     def setCartesianCoord(self):
         self.posX = self.dist * math.cos(self.angle)
         self.posY = self.dist * math.sin(self.angle)
 
-    def rotate(self, angle):
-        self.angle += angle
-        self.setCartesianCoord()
+    def calcDistance(self, point):
+        return 1
 
 class   Planet(Point):
     def __init__(self, name, dist, mass, angle, vel):
         self.name = name
         self.mass = mass
-        super().__init__(dist, angle, vel)
+        self.vel = vel
+        super().__init__(dist, angle)
+
+    def rotate(self, time):
+        self.angle += self.vel * time
+        self.setCartesianCoord()
         
 class   Satellite(Point):
     def __init__(self, home):
         self.dist = home.dist
-        super().__init__(home.dist, home.angle, home.vel)
+        super().__init__(home.dist, home.angle)
         self.angle = home.angle - home.vel
+        self.setCartesianVel(1.0)#difference in position of the sat and the planet
 
-    def calc_dist(planet):
+    def setCartesianVel(self, vel):
+        self.velX = vel * math.cos(self.angle)
+        self.velY = vel * math.sin(self.angle)
+
+    def calcDist(self, planet):
+        return self.calcDistance(planet)
+
+    def getForce(self, planet):
         return 1
 
-    def calc_force(planet):
-        dist = calc_dist(planet)
-        return G * planet.mass / (dist * dist)
+    def getAcceleration(self, planet, friction):
+        return 1
+
+    def updatePosition(self, planet, friction):
+        return 1
 
 Sun = Planet("Sun", 0.0, 100.0, 0.0, 0.0)
 Mercury = Planet("Mercury", 100.0, 20.0, math.pi / 2.0, 2.0 * math.pi / 288)
@@ -53,10 +66,11 @@ def simulate(velX, velY, friction):
     success = False
     while t < T and not success:
         for body in bodies:
-            dist[body.name] = sat.dist - body.dist
-            if dist[body.name] == 0:
-                print(body.name)
+            sat.updatePosition(body, friction)
+            print(body.name)
+            print("X = %f" % body.posX)
+            print("Y = %f" % body.posY)
         success = True
         t += step
-        
-simulate(0.0, 0.0, 0.0)
+
+simulate(1.0, 1.0, 1.0)
