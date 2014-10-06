231aa4a3e26501d106396816395f905882d1d849
Thomas Marcoux <txmarcoux@ualr.edu>
2014-10-05 20:50:56 -0600

    update

--- red_planet/eval.py
+++
@@ -1,5 +0,0 @@
-import math
-
-def evaluate(param):
-    cost = math.pow(10, 5) + math.pow(10, 4) + 10
-    return cost
--- red_planet/main.py
+++
@@ -1,7 +0,0 @@
-import sim
-import search
-
-S = 500
-
-search.search(S)
-#sim.simulate(1.0, 1.0, 1.0)
