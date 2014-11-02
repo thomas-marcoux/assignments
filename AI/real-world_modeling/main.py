fe730d5a010eb267cc2774ba310fd96dc85ab32a
thomas-marcoux <thomas.marcoux.graphi@gmail.com>
2014-11-01 21:51:18 -0500

    added parent function

--- AI/real-world_modeling/main.py
+++ AI/real-world_modeling/main.py
@@ -1,6 +1,7 @@
 import prior_sampling
 import rejection_sampling
 import answers
+import network
 
-prior_sampling.prior_sampling(answers.answs)
+prior_sampling.prior_sampling(answers.answs, network.net)
 #rejection_sampling.rejection_sampling(answers.answs)
--- AI/real-world_modeling/prior_sampling.py
+++ AI/real-world_modeling/prior_sampling.py
@@ -1,15 +1,23 @@
-import network
 import random
+
+def     parent(network, event):
+    p = []
+    for i in network:
+        for j in network[i]:
+            if (event == j):
+                p += i
+    return p
 
 def     sample(answer, event, parent):
     print(answer[event])
     print(answer[parent])
 
-def     prior_sampling(answers):
+def     prior_sampling(answers, network):
     x = {}
+    print(parent(network, 'D'))
     for i in answers:
         n = random.randrange(len(answers)) + 1
         x[i] = answers[n]
-        sample(answers[n], 'D', 'C')
+        #sample(answers[n], 'D', 'C')
     return x
 
