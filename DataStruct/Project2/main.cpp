78e64cb542e2eb4bef31ad4732d10d88af01c55f
thomas-marcoux <thomas.marcoux.graphi@gmail.com>
2014-11-02 21:29:35 -0600

    Added header comment

--- DataStruct/Project2/main.cpp
+++ DataStruct/Project2/main.cpp
@@ -1,3 +1,15 @@
+/* CPSC2380 
+ * Department of Computer Science, UALR 
+ * Project 2 
+ * Student Name: Thomas Marcoux
+ * Student UALR ID (last four digits): 5564
+ * Project Descriptions: 
+ * This project takes an arithmetic expression as input,
+ * converts it to postfix and evaluates it, outputting the result.
+ * Project Due Date: 11/4/2014 
+ * Project Revised Date: 8/30/2014, 11/3/2014 
+ */
+
 #include <iostream>
 #include "tokenList.h"
 
